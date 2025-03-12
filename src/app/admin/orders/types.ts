44fc63ac81547614e7aeb52cddb9cc09b8e70e79
Graham-ki <grahamkimbugwe1738@gmail.com>
2025-03-12 13:49:29 +0300

    Update types.ts again

--- src/app/admin/orders/types.ts
+++ src/app/admin/orders/types.ts
@@ -6,6 +6,6 @@ const supabase = await createClient();
 const ordersWithProductsQuery = supabase
   .from('order')
   .select('*, order_items:order_item(*, product(*)), user(*)')
-  .or('status.eq.Approved,status.eq.Pending') // Filter for status = 'Approved' or 'Pending'
+  .eq(status,'Approved') // Filter for status = 'Approved' or 'Pending'
   .order('created_at', { ascending: false });
 export type OrdersWithProducts = QueryData<typeof ordersWithProductsQuery>;
